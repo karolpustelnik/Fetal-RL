fixed normalization and scaler paths

swin-transformer/data/fetal_loader.py
@@ -22,6 +22,11 @@ warnings.filterwarnings("ignore", "(Possibly )?corrupt EXIF data", UserWarning)
 5 - femur non standard plane
 6 - femur standard plane
 """
+
+def clamp(num, min_value, max_value):
+   return max(min(num, max_value), min_value)
+
+
 class Fetal_frame_eval(data.Dataset):
     def __init__(self, root, ann_path, transform=None, target_transform=None):
 
@@ -116,6 +121,7 @@ class Fetal_frame(data.Dataset):
         self.target_transform = target_transform
         self.database = pd.read_csv(self.ann_path)
         self.img_scaling = img_scaling
+        
 
     def _load_image(self, path):
         try:
@@ -147,9 +153,12 @@ class Fetal_frame(data.Dataset):
         width = torch.tensor(idb[10])
         measure_normalized = torch.tensor(idb[13], dtype=torch.float32)
         images = self._load_image(self.data_path  + frame_idx + '.png')
+        
+        new_height = clamp(int(height*2.161191086437513), 0, 512)
+        new_width = clamp(int(width*2.161191086437513), 0, 512)
         if self.img_scaling:
             padding = A.PadIfNeeded(min_height=512, min_width=512, border_mode=0, value=0, mask_value=0, always_apply=False, p=1.0)
-            rescale = A.Resize(int(1.62*height), int(1.62*width))
+            rescale = A.Resize(new_height, new_width)
             images = rescale(image = images)['image']
             images = padding(image=images)['image']
             images = np.expand_dims(images, 2)

swin-transformer/main.py
@@ -71,6 +71,8 @@ def parse_option():
 
     # distributed training
     parser.add_argument("--local_rank", type=int, required=False, help='local rank for DistributedDataParallel')
+    parser.add_argument("--train_path", type=str, help='path to training data')
+    parser.add_argument("--val_path", type=str, help='path to validation data')
     parser.add_argument("--path_prefix", type=str, help='path prefix eq. /home/karol')
     # for acceleration
     parser.add_argument('--fused_window_process', action='store_true', help='Fused window shift & window partition, similar for reversed part.')
@@ -350,10 +352,8 @@ def validate(config, data_loader, model):
             measures_train = measure_normalized if config.DATA.IMG_SCALING else measure_scaled
             measures_train = measures_train.unsqueeze(0).cuda(non_blocking=True)
             measures_train = measures_train.reshape(-1, 1)
-            print('measure train shape', measures_train.shape)
             #print('outputs shape', outputs.shape)
             #print('outputs', outputs)
-            print('measures train', measures_train)
             #with torch.autocast(device_type='cuda', dtype=torch.float16):
             loss_reg = criterion_reg(outputs, measures_train)
             #ps = ps.cuda(non_blocking=True)
@@ -361,10 +361,10 @@ def validate(config, data_loader, model):
             ps = ps.cpu().numpy()
             prefix = config.DATA.PATH_PREFIX
             if config.DATA.IMG_SCALING:
-                scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/scripts/normalizer_measure')
+                scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/normalizer_measure_img_scaling')
                 predicted_measure = scaler.inverse_transform(outputs.cpu().numpy())
             else:
-                scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/scripts/normalizer_measure')
+                scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/normalizer_measure')
                 predicted_measure = scaler.inverse_transform(outputs.cpu().numpy()) * ps
             predicted_measure = torch.from_numpy(predicted_measure)
             predicted_measure = predicted_measure.cuda(non_blocking=True)
@@ -374,10 +374,6 @@ def validate(config, data_loader, model):
             mae_value = mae(predicted_measure, measure)
             mape_value = mape(predicted_measure, measure)
             rmse_value = rmse(predicted_measure, measure)
-            print(f'Predicted measure tensor: {predicted_measure}')
-            print(f'Measure tensor: {measure}')
-            
-            print(f"Predicted measure: {predicted_measure[0]}, real measure: {measure[0]}, mae: {mae_value}, mape: {mape_value}, rmse: {rmse_value}")
             
             if config.PARALLEL_TYPE == 'ddp':
                 loss_reg = reduce_tensor(loss_reg)
@@ -395,6 +391,7 @@ def validate(config, data_loader, model):
 
         if idx % config.PRINT_FREQ == 0:
             if config.MODEL.TASK_TYPE == 'cls':
+                
                 logger.info(
                     f'Test: [{idx}/{len(data_loader)}]\t'
                     f'cls Loss {loss_meter_cls.val:.4f} ({loss_meter_cls.avg:.4f})\t' 
@@ -403,6 +400,7 @@ def validate(config, data_loader, model):
                     f'recall {recall_meter.val:.3f} ({recall_meter.avg:.3f})\t' 
                     f'precision {precision_meter.val:.3f} ({precision_meter.avg:.3f})\t')
             elif config.MODEL.TASK_TYPE == 'reg':
+                print(f"Example measure prediction: {predicted_measure[0]}, real measure: {measure[0]}, mae: {mae_value}, mape: {mape_value}, rmse: {rmse_value}")
                 logger.info(
                     f'Test: [{idx}/{len(data_loader)}]\t'
                     f'reg Loss {loss_meter_reg.val:.4f} ({loss_meter_reg.avg:.4f})\t' 

swin-transformer/models/effnetv2.py
@@ -94,13 +94,13 @@ class EffnetV2_L_meta(torch.nn.Module):
         self.model.features[0] = torch.nn.Conv2d(self.in_channels, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
         self.model.classifier = torch.nn.Identity()
         #self.model.classifier = torch.nn.Sequential(nn.Dropout(0.4), nn.Linear(1280, self.out_features))
-        self.classifier = torch.nn.Sequential(nn.Linear(1280 + 64, 512),
+        self.classifier = torch.nn.Sequential(nn.Linear(1280 + 4, 512),
                                              nn.BatchNorm1d(512),
                                              torch.nn.SiLU(),
                                              nn.Dropout(self.dropout),
                                              torch.nn.Linear(512, out_features = self.out_features),) # 1280 + 64 meta feature (days, frame_location)
-        self.meta = torch.nn.Sequential(nn.Linear(2, 64),
-                                        nn.BatchNorm1d(64),
+        self.meta = torch.nn.Sequential(nn.Linear(2, 4),
+                                        nn.BatchNorm1d(4),
                                         nn.SiLU(),
                                         nn.Dropout(self.dropout))
                                         