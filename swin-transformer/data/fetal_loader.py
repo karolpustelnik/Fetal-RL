fixed normalization

swin-transformer/data/fetal_loader.py
@@ -38,7 +38,7 @@ class Fetal_frame_eval(data.Dataset):
 
     def _load_image(self, path):
         try:
-            im = Image.open(path)
+            im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         except:
             print("ERROR IMG NOT LOADED: ", path)
             random_img = np.random.rand(224, 224, 3) * 255
@@ -54,16 +54,21 @@ class Fetal_frame_eval(data.Dataset):
         """
         idb = self.database.iloc[index]
         frame_idx = idb[0]
-        ps = idb[2]
+        ps = idb[3]
         video = idb[1]
-        Class = idb[3]
+        Class = idb[2]
         #ps = torch.tensor(idb[4])
 
         images = self._load_image(self.data_path  + frame_idx + '.png')
+        images = np.expand_dims(images, 2)
+        t = transforms.Compose([transforms.ToTensor(),
+        transforms.Resize((450, 600)),
+        transforms.Pad((0, 0, 0, 150), fill = 0, padding_mode = 'constant'),
+        transforms.Resize((512, 512)),
+        transforms.Normalize(mean=0.1354949, std=0.18222201)])
+        images = t(images)
         if self.transform is not None:
             images = self.transform(images)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
         
         return images, frame_idx, video, ps, Class
 

swin-transformer/main.py
@@ -361,7 +361,14 @@ def validate(config, data_loader, model):
                 scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/normalizer_measure_img_scaling')
                 predicted_measure = scaler.inverse_transform(outputs.cpu().numpy())
             else:
-                scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/all/all_scaler')
+                if config.MODEL.BODY_PART == 'head':
+                    scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/head/head_scaler')
+                elif config.MODEL.BODY_PART == 'abdomen':
+                    scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/abdomen/abdomen_scaler')
+                elif config.MODEL.BODY_PART == 'femur':
+                    scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/femur/femur_scaler')
+                elif config.MODEL.BODY_PART == 'all':
+                    scaler = joblib.load(f'{prefix}/kpusteln/Fetal-RL/data_preparation/data_biometry/ete_model/biometry_scaled_ps/all/all_scaler')
                 predicted_measure = scaler.inverse_transform(outputs.cpu().numpy()) * ps
             predicted_measure = torch.from_numpy(predicted_measure)
             predicted_measure = predicted_measure.cuda(non_blocking=True)